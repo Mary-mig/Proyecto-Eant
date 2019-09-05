62923347966f071c3ec5e9fcda75fa3b3b2e1562
mary-mig <maria.miglio84@gmail.com>
2019-09-04 22:03:53 -0300

    Agregando validacion a hipervinculos con JS

---
+++ Ejercicios JS/validar-link.js
@@ -0,0 +1,23 @@
+let links = document.querySelectorAll("nav a")
+
+console.log(links)
+
+links.forEach( manipularLink )
+
+function manipularLink (link){
+	//Aca voy a manipularlLink el link..
+	console.log (link)
+
+	link.onclick = validar
+}
+
+function validar (evento){
+	console.log (evento)
+	evento.preventDefault()//<-- Frenar el comportamiento predeterminado del link/hipervinculo
+	let rta = confirm("Esta seguro que desea salir de la pagina?")
+
+	if (rta) {
+		alert (" Bueno ..chau")
+	}
+
+}
